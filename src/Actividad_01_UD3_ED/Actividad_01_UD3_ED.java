5843ed7a0889e0587b1b1f8cbb60ed9dcb8d2413
Andres <amangil1010@g.educaand.com>
2021-12-13 14:34:41 +0100

    Subida con la Refactorizacion hecha AMG

--- src/Actividad_01_UD3_ED/Actividad_01_UD3_ED.java
+++ src/Actividad_01_UD3_ED/Actividad_01_UD3_ED.java
@@ -16,24 +16,24 @@ import java.io.IOException;
  */
 public class Actividad_01_UD3_ED 
 {
-    private static byte[] buffer = new byte[1000];
-    private static String fileName = "fichero.dat";
-    private static FileInputStream inputStream = null;
-    private static BufferedInputStream inputBuffer = null;
+    private static byte[] intermedio = new byte[1000];
+    private static String Nombredelarchivo = "fichero.dat";
+    private static FileInputStream flujodeentrada = null;
+    private static BufferedInputStream búferdeentrada = null;
 
     public static void inicializateFiles() throws FileNotFoundException
     {
-        inputStream = new FileInputStream(fileName);
-        inputBuffer = new BufferedInputStream(inputStream);
+        flujodeentrada = new FileInputStream(Nombredelarchivo);
+        búferdeentrada = new BufferedInputStream(flujodeentrada);
     }
     
     public static int showFileText() throws IOException
     {
         int total = 0;
         int nRead = 0;
-        while((nRead = inputStream.read(buffer)) != -1) 
+        while((nRead = flujodeentrada.read(intermedio)) != -1) 
         {
-            System.out.println(new String(buffer));
+            System.out.println(new String(intermedio));
             total += nRead;
         }
         
@@ -61,10 +61,10 @@ public class Actividad_01_UD3_ED
         {
             try 
             {
-                if( inputBuffer != null && inputStream != null )
+                if( búferdeentrada != null && flujodeentrada != null )
                 {
-                    inputStream.close();
-                    inputBuffer.close();
+                    flujodeentrada.close();
+                    búferdeentrada.close();
                 }                
             } 
             catch (IOException ex) 
